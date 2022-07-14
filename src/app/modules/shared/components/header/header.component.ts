9bc0575fd7cf4f7fc6c3f0908a59ae62dba83209
Daniel Lopez <daniellopezj0327@gmail.com>
2022-07-13 19:01:16 -0500

    start with header

--- src/app/modules/shared/components/header/header.component.ts
+++ src/app/modules/shared/components/header/header.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+
+  Inject,
+  PLATFORM_ID,
+  HostListener,
+  Input,
+} from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -7,10 +18,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Input() solid: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let element = document.querySelector('.main-container') as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('scroll-nav');
+    } else {
+      element.classList.remove('scroll-nav');
+    }
+  }
+  itemsHeader = [
+    {
+      title: 'Pagos',
+      route: ['plans']
+    },
+    {
+      title: 'Nosotros',
+      route: ['about']
+    },
+    {
+      title: 'Ayuda',
+      route: ['help']
+    },
+    {
+      title: 'Pagar Factura',
+      route: ['payments'],
+      active: true
+    }
+  ]
+
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
-    console.log('this is header')
+  }
+
+  goTo(route: string[]) {
+    this.router.navigate(route)
   }
 
 }
