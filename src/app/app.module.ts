c4d13ef33119bf2d21f735a270ccd875c9f72595
Daniel Lopez <daniellopezj0327@gmail.com>
2022-07-18 17:20:15 -0500

    completed footer and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './modules/shared/shared.module';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +11,7 @@ import { SharedModule } from './modules/shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/shared/components/footer/footer.component.ts
+++ src/app/modules/shared/components/footer/footer.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn, faGithub, faGitlab, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  public controls = [
+    {
+      icon: faTwitter,
+      link: ''
+    },
+    {
+      icon: faLinkedinIn,
+      link: ''
+    },
+    {
+      icon: faGithub,
+      link: ''
+    },
+    {
+      icon: faGitlab,
+      link: ''
+    },
+    {
+      icon: faInstagram,
+      link: ''
+    },
+    {
+      icon: faEnvelope,
+      link: ''
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
