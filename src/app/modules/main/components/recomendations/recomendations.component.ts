aa65637b2f3532e28a3c66ca0485a3d3424d1b5d
Daniel Lopez <daniellopezj0327@gmail.com>
2022-07-27 17:45:24 -0500

    create testimony

--- src/app/modules/main/components/recomendations/recomendations.component.ts
+++ src/app/modules/main/components/recomendations/recomendations.component.ts
@@ -1,5 +1,5 @@
 import { Testimony } from '@/types/general.types';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-recomendations',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recomendations.component.scss']
 })
 export class RecomendationsComponent implements OnInit {
-
+  @ViewChild('testimonyComponent') testimonyComponent: ElementRef | undefined;
   public testimonies: Testimony[] = [
     {
       id: 0,
@@ -23,8 +23,23 @@ export class RecomendationsComponent implements OnInit {
       name: 'Pascal Tremblay',
       profession: 'Creative Lead, Good Kind'
     },
+    {
+      id: 2,
+      avatar: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Timisoara_-_Regional_Business_Centre.jpg',
+      description: 'Matt was a real pleasure to work with and we look forward to working with him again. He’s definitely the kind of designer you can trust with a project from start to finish.',
+      name: 'Pascal Tremblay',
+      profession: 'Creative Lead, Good Kind'
+    },
+    {
+      id: 3,
+      avatar: 'https://speakzeasy.files.wordpress.com/2015/05/twa_blogpic_timisoara-4415.jpg',
+      description: 'Matt was a real pleasure to work with and we look forward to working with him again. He’s definitely the kind of designer you can trust with a project from start to finish.',
+      name: 'Pascal Tremblay',
+      profession: 'Creative Lead, Good Kind'
+    },
   ]
   private currentIndex = 0
+  public activeDot = 0
   public currentTestimony = this.testimonies[this.currentIndex]
 
   constructor() { }
@@ -48,5 +63,17 @@ export class RecomendationsComponent implements OnInit {
       this.currentIndex--
     }
     this.currentTestimony = this.testimonies[this.currentIndex]
+  }
+
+  controlDot(position: number) {
+    if (this.testimonyComponent) {
+
+      this.testimonyComponent.nativeElement.classList.add('fade')
+      setTimeout(() => {
+        this.testimonyComponent?.nativeElement.classList.remove('fade')
+      }, 2000);
+    }
+    this.activeDot = position
+    this.currentTestimony = this.testimonies[this.activeDot]
   }
 }
