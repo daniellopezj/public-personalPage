921faf55036a2da6ab6127fe85a796312caf5c37
Daniel Lopez <daniellopezj0327@gmail.com>
2023-07-13 15:07:08 -0500

    fix bug in translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     FontAwesomeModule,
     HttpClientModule,
     TranslateModule.forRoot({
-      defaultLanguage: isLanguageSupported(navigator.language) ? navigator.language.split('-')[0] : 'en',
+      defaultLanguage: 'en',
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
@@ -38,8 +38,3 @@ export function HttpLoaderFactory(http: HttpClient) {
   bootstrap: [AppComponent,]
 })
 export class AppModule { }
-
-function isLanguageSupported(language: string): boolean {
-  const supportedLanguages = ['en', 'es'];
-  return supportedLanguages.includes(language.split('-')[0]);
-}
